8f99953adc7705475924f79a9f583693fb62be95
JKjiwon <jwkim.oa@gmail.com>
2023-07-30 14:40:39 +0900

    StoneWall 리펙토링

--- src/main/kotlin/codility/stackandqueue/StoneWall.kt
+++ src/main/kotlin/codility/stackandqueue/StoneWall.kt
@@ -6,14 +6,15 @@ class StoneWall {
     fun solution(H: IntArray): Int {
         val stack = Stack<Int>()
         var count = 0
-        for (i in H.indices) {
+
+        for (height in H) {
             // 현재 높이보다 높은 벽 제거
-            while (stack.isNotEmpty() && stack.peek() > H[i]) {
+            while (stack.isNotEmpty() && stack.peek() > height) {
                 stack.pop()
             }
             // 새로운 벽을 쌓는다.
-            if (stack.isEmpty() || stack.isNotEmpty() && stack.peek() < H[i]) {
-                stack.push(H[i])
+            if (stack.isEmpty() || stack.isNotEmpty() && stack.peek() < height) {
+                stack.push(height)
                 count++
             }
         }
