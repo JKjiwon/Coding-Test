7f24de9575a57b995de942a72d3d79b3aa3aa6be
JKjiwon <jwkim.oa@gmail.com>
2023-08-12 20:29:09 +0900

    [이것이 코딩테스트다] 10. grape::union-find

---
+++ src/main/kotlin/thisiscodingtest/grape/UnionFind.java
@@ -0,0 +1,14 @@
+package thisiscodingtest.grape;
+
+import java.util.Scanner;
+
+public class UnionFind {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int v = scanner.nextInt();
+        int e = scanner.nextInt();
+        System.out.println(v);
+        System.out.println(e);
+    }
+}
